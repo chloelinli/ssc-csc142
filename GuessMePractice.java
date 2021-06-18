404aa5d4fbbad62688ff1b7a4dd7d5b1a6022661
chloe <rushcl20@cs.washington.edu>
2021-06-18 14:09:04 -0700

    revised GuessMePractice.java, some bugs due to simplicity (lots of work to workout special bugs)

--- GuessMePractice.java
+++ GuessMePractice.java
@@ -1,128 +1,125 @@
-// Chloe Rushing
-// 7/27/19
-// This program is a guessing game in which the user wins when they guess the secret number within the limited number of guesses.
-
-//package test;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class GuessMePractice {
-	
-	public static boolean playGame(Scanner console) {
-		boolean guessed = false;
+
+	public static void main(String[] args) {
+		// initialize scanner and prints
+		Scanner console = new Scanner(System.in);
+		System.out.println("Hi -- Welcome to the Guessing Game.");
+		System.out.println("To play you need to guess the magic number.");
+
+		// boolean for play - initially true for one game
+		boolean play = true;
+
+		// while play
+		while (play) {
+			// play game
+			playGame(console);
+
+			// ask for play
+			String keepPlay = "";
+			System.out.print("\nWould you like to play again? [yes/no]: ");
+			keepPlay = console.next().toLowerCase();
+			char[] keepArr = keepPlay.toCharArray();
+
+			// while answer not y/n, loop
+			while (keepArr[0] != 'y' && keepArr[0] != 'n') {
+				System.out.print("Invalid input, please try again: ");
+				keepPlay = console.next().toLowerCase();
+				keepArr = keepPlay.toCharArray();
+			}
+
+			// if n boolean is false
+			if (keepPlay.charAt(0) == 'n') {
+				play = false;
+				break;
+			}
+		}
+
+		// print ty
+		System.out.println("\nThank you for playing this guessing game.");
+	}
+
+	public static void playGame(Scanner console) {
+		// boolean for correct
+		boolean correct = false;
+
+		// print statements for play
 		System.out.println("Enter a range of numbers that you would like to use.");
-		System.out.print("(Enter with one space between; must have at least 4 numbers in between) Ex. 0 10: ");
-		
+		System.out.println("\tFormat: <#><space><#>");
+		System.out.println("\tMust have at least 4 numbers within range. Ex: 0 10");
+
+		// set range - check for invalid range
 		int min = console.nextInt();
 		int max = console.nextInt();
 		int diff = max - min;
 		while (diff < 4) {
-			System.out.print("Invalid range. Please try again: ");
+			System.out.print("Invalid range, please try again. Valid format is <#><space><#>: ");
 			min = console.nextInt();
 			max = console.nextInt();
 			diff = max - min;
 		}
+
+		// initialize answer, guess and prevGuess and things
 		int guessCount = 0;
-		int numGuess = (max-min)/2;
-		System.out.println("\nYour guess should be between " + min + " and " + max + ", inclusive.");
-		System.out.println("You get a total of " + numGuess + " guesses.");
-		int randomNum =  getRandomIntegerBetweenRange(min, max);
-		System.out.print("Would you like to continue playing? ");
-		String reply = console.next().toLowerCase();
-		while (!reply.equals("y") && !reply.equals("yes") && !reply.equals("n") && ! reply.equals("no)")) {
-			System.out.print("Invalid input. Please try again: ");
-			reply = console.next().toLowerCase();
-		}
-		if (reply.equals("n") || reply.equals("no")) {
-			guessed = true;
-		}
-		
-		while (!guessed && (guessCount < numGuess) && (reply.equals("y") || reply.equals("yes"))) {
+		int maxGuess = diff / 2;
+		int answer = getRandomIntInRange(min, max);
+		int currGuess = 0;
+		int prevGuess = answer;
+
+		// while not max guesses or not correct
+		while (guessCount < maxGuess || !correct) {
+			// get user guess
+			System.out.println("\nYour guess should be between " + min + " and " + max + ", inclusive.");
+			System.out.println("You have " + (maxGuess - guessCount) + " guess(es).");
+			currGuess = console.nextInt();
+
+			// if not in range loop for valid guess
+			while (currGuess > max || currGuess < min) {
+				System.out.print("Beyond range, please guess within " + min + " to " + max + ": ");
+				currGuess = console.nextInt();
+			}
+
+			// count++
 			guessCount++;
-			System.out.print("\nGuess #" + guessCount + ": ");
-			int guess = console.nextInt();
-			while (guess < min || guess > max) {
-				System.out.print("\nBeyond range. Please try again: ");
-				guess = console.nextInt();
+
+			// if correct change boolean
+			if (currGuess == answer) {
+				correct = true;
+				break;
 			}
-			int hint = Math.abs((randomNum - guess));
-			if (hint >= 1 && hint <= 2) {
-				System.out.println("Warmer...Closer...You're getting there!");
-			}
-			else if (hint > (diff/10) && hint <= diff) {
-				System.out.println("Yikes! Too far!");
-			}
+
+			// else use prevGuess and print statements
 			else {
-				if (guess != randomNum) {
-					System.out.println("Not quite there yet!");
+				int dist = Math.abs(answer - currGuess);
+				if (dist == 1) {
+					System.out.println("Close!");
+				} else if (dist < Math.abs(answer - prevGuess) && guessCount > 1) {
+					System.out.println("Getting warmer!");
+				} else if (dist + 1 == Math.abs(answer - prevGuess) && guessCount > 1) {
+					System.out.println("Getting colder :(");
+				} else {
+					System.out.println("Not quite there.");
 				}
 			}
-			if (guess == randomNum) {
-				guessed = true;
-				System.out.println("Congratulations, you found the magic number!");
+			if (guessCount == maxGuess) {
+				break;
 			}
-			else if (guessCount == numGuess && !guessed) {
-				guessed = true;
-				System.out.println("Sorry, you did not guess the number.");
-			}
-			else if (!guessed && guessCount < numGuess) {
-				System.out.print("Would you like to continue the current game? ");
-				reply = console.next().toLowerCase();
-				while (!reply.equals("y") && !reply.equals("yes") && !reply.equals("n") && ! reply.equals("no)")) {
-					System.out.print("Invalid input. Please try again: ");
-					reply = console.next().toLowerCase();
-				}
-				if (reply.equals("n") || reply.equals("no")) {
-					guessed = true;
-				}
-			}
+
+			prevGuess = currGuess;
 		}
-		return guessed;
-	}
-	
-	public static int getRandomIntegerBetweenRange(double min, double max){
-		int x = (int) ((Math.random()*((max-min)+1))+min);
-		return x;
+
+		// if not correct print sorry
+		if (!correct) {
+			System.out.println("Sorry, you did not guess the number.");
+		}
+		// else print congrats
+		else {
+			System.out.println("Congratulations, you found the magic number!");
+		}
 	}
 
-	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
-		System.out.println("Hi -- Welcome to the Guessing Game.");
-		System.out.println("To play you need to guess the magic number.");
-		boolean result = playGame(console);
-		String ans = "";
-		
-		while (result == true) {
-			System.out.print("\nWould you like to play again? Yes or No? ");
-			ans = console.next().toLowerCase();
-			if (ans.length() == 1) {
-				while (!ans.equals("y") && !ans.equals("n")) {
-					System.out.print("Invalid input. Please try again: ");
-					ans = console.next().toLowerCase();
-				}
-				if (ans.equals("y")) {
-					System.out.println();
-					result = playGame(console);
-				}
-				else if (ans.equals("n")) {
-					result = false;
-				}
-			}
-			else {
-				while (!ans.equals("yes") && !ans.equals("no")) {
-					System.out.print("Invalid input. Please try again: ");
-					ans = console.next().toLowerCase();
-				}
-				if (ans.equals("yes")) {
-					System.out.println();
-					result = playGame(console);
-				}
-				else if (ans.equals("no")) {
-					result = false;
-				}
-			}
-		}
-		
-		System.out.println("\nThank you for playing this guessing game.");
+	public static int getRandomIntInRange(int min, int max) {
+		return (int) Math.floor(Math.random()*(max-min+1)+min);
 	}
 }
